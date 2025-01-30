74fcb5924971983c67273398db145f3e8ccbf296
Admin <Admin@DESKTOP-NFLJM9B>
2025-01-30 19:23:18 +0530

    adding home controller

--- RideSharingApplicationBackend/src/main/java/com/cdac/ridesharingapplication/controller/HomeController.java
+++ RideSharingApplicationBackend/src/main/java/com/cdac/ridesharingapplication/controller/HomeController.java
@@ -3,8 +3,11 @@ package com.cdac.ridesharingapplication.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+* Author: Admin
+* Date: 30 Jan 2025
+**/
 @RestController
-@RequestMapping("/ride")
-public class RideController {
-  
+@RequestMapping("/home")
+public class HomeController {
 }

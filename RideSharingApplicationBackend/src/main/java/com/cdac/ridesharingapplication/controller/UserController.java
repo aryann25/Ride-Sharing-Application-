04dc3f7074d56845c55f6f50f7b81adc324251c4
Admin <Admin@DESKTOP-NFLJM9B>
2025-01-30 18:15:54 +0530

    adding driver and user controller

---
+++ RideSharingApplicationBackend/src/main/java/com/cdac/ridesharingapplication/controller/UserController.java
@@ -0,0 +1,14 @@
+package com.cdac.ridesharingapplication.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+* Author: Admin
+* Date: 30 Jan 2025
+**/
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+}

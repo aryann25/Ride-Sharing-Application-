1f07aff2251a40237b5bfea451352fa93569566a
hp <hp@DESKTOP-AKBI38C>
2025-01-30 19:16:08 +0530

    adding ride controller

---
+++ RideSharingApplicationBackend/src/main/java/com/cdac/ridesharingapplication/controller/RideController.java
@@ -0,0 +1,10 @@
+package com.cdac.ridesharingapplication.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/ride")
+public class RideController {
+
+}

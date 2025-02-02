6ca1b30df2f7d585902f0831e5b793beb84484cf
aryan <aryan@Asus16x>
2025-02-02 18:10:37 +0530

    Added User login and register

--- RideSharingApplicationBackend/src/main/java/com/cdac/ridesharingapplication/controller/UserController.java
+++ RideSharingApplicationBackend/src/main/java/com/cdac/ridesharingapplication/controller/UserController.java
@@ -1,14 +1,30 @@
 package com.cdac.ridesharingapplication.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import com.cdac.ridesharingapplication.model.User;
+import com.cdac.ridesharingapplication.service.UserService;
+import com.cdac.ridesharingapplication.dto.UserDTO;
+import com.cdac.ridesharingapplication.dto.AuthResponse;
+import com.cdac.ridesharingapplication.dto.LoginDTO;
 
-/**
-* Author: Admin
-* Date: 30 Jan 2025
-**/
 @RestController
 @RequestMapping("/user")
-public class UserController {
-
+public class UserController 
+{
+	 @Autowired
+	    private UserService userService;
+	    
+	    @PostMapping("/register")
+	    public ResponseEntity<AuthResponse> registerUser(@RequestBody UserDTO userDTO) {
+	        AuthResponse response = userService.registerUser(userDTO);
+	        return ResponseEntity.ok(response);
+	    }
+	    
+	    @PostMapping("/login")
+	    public ResponseEntity<AuthResponse> loginUser(@RequestBody LoginDTO loginDTO) {
+	        AuthResponse response = userService.loginUser(loginDTO);
+	        return ResponseEntity.ok(response);
+	    }
 }
--- RideSharingApplicationBackend/src/main/java/com/cdac/ridesharingapplication/model/User.java
+++ RideSharingApplicationBackend/src/main/java/com/cdac/ridesharingapplication/model/User.java
@@ -34,4 +34,60 @@ public class User {
 	  @JsonIgnore
 	    @OneToOne(cascade = CascadeType.ALL)
 	    private Ride currentRide;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getFullName() {
+		return fullName;
+	}
+
+	public void setFullName(String fullName) {
+		this.fullName = fullName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getMobile() {
+		return mobile;
+	}
+
+	public void setMobile(String mobile) {
+		this.mobile = mobile;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getProfilePicture() {
+		return profilePicture;
+	}
+
+	public void setProfilePicture(String profilePicture) {
+		this.profilePicture = profilePicture;
+	}
+
+	public Ride getCurrentRide() {
+		return currentRide;
+	}
+
+	public void setCurrentRide(Ride currentRide) {
+		this.currentRide = currentRide;
+	}
 }
